c107cdbb32db4ed346ed9c89cf63bb8c2121339c
n4to4 <n4to4k@gmail.com>
2022-06-02 22:24:37 +0900

    init

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const date_fns_1 = require("date-fns");
+const d = (0, date_fns_1.format)(new Date(), "yyyy-MM-dd");
+console.log(d);
---
+++ main.ts
@@ -0,0 +1,4 @@
+import { format } from "date-fns";
+
+const d = format(new Date(), "yyyy-MM-dd");
+console.log(d);
