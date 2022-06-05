48c0968491919271a23243a93497c13e959f84a4
n4to4 <n4to4k@gmail.com>
2022-06-05 10:48:29 +0900

    wip

--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
-import { format } from "date-fns";
+import { parseISO, format } from "date-fns";
+import locale from "date-fns/locale/ja";
 
-const d = format(new Date(), "yyyy-MM-dd");
-console.log(d);
+interface Parsed {
+  original: string;
+  parsed: Date;
+}
+
+const args = process.argv.slice(2);
+const dates: Parsed[] = args.map((arg) => ({ original: arg, parsed: parseISO(arg) }));
+for (const p of dates) {
+  const jst = format(p.parsed, "yyyy-MM-dd'T'HH:mm:ssXXX", { locale: locale });
+  console.log(`${p.original}  =>  ${jst}`);
+}
